67bccae54dd2f6ede8f96146a3d213e97a2e7bd1
michaeldel <michaeldel@protonmail.com>
2017-11-17 07:15:06 +0100

    Get initial using axios

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
         </p>
       </div>
       <div class="container">
-        <table class="table">
+        <p v-if="loading">Loading data...</p>
+        <table class="table" v-else>
           <thead>
             <tr>
               <th><abbr title="Base">B</abbr> \ <abbr title="Quote">Q</abbr></th>
@@ -34,21 +35,46 @@ export default {
   name: 'app',
   data () {
     return {
-      currencies: ['BTC', 'LTC', 'ETH', 'XRP', 'USD', 'EUR'],
-      tickers: []
+      currencies: [],
+      tickers: [],
+      loading: true
     }
   },
 
-  beforeMount() {
-    for (let i in this.currencies) {
-      this.tickers.push([])
-      for (let _ in this.currencies)
-        this.tickers[i].push(undefined)
-    }
+  mounted() {
+    let markets = []
 
-    for (let i = 0; i < this.currencies.length; i++)
-      for (let j = i + 1; j < this.currencies.length; j++)
-        this.setTicker(i, j, 20 * Math.random())
+    this.$http.get('https://www.bitstamp.net/api/v2/trading-pairs-info/').then(response => {
+      for (const pair of response.data.map(d => d.name.split('/')).sort())
+        for (const currency of pair)
+          if (this.currencies.indexOf(currency) === -1)
+            this.currencies.push(currency)
+
+      markets = response.data.map(d => d.name)
+
+      for (let i in this.currencies) {
+        this.tickers.push([])
+
+        for (let _ in this.currencies)
+          this.tickers[i].push(undefined)
+      }
+
+      const promises = []
+      for (const market of markets) {
+        const pair = market.split('/')
+        const base = pair[0]
+        const quote = pair[1]
+        promises.push(this.$http.get(`https://www.bitstamp.net/api/v2/ticker_hour/${market.split('/').map(m => m.toLowerCase()).join('')}/`).then(response => {
+          const i = this.currencies.indexOf(quote)
+          const j = this.currencies.indexOf(base)
+          this.setTicker(i, j, response.data.last)
+        }))
+      }
+
+      Promise.all(promises).then(_ => {this.loading = false})
+    }).catch(error => {
+      console.error(error)
+    })
   },
 
   methods: {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import axios from 'axios'
 import Vue from 'vue'
+
 import App from './App.vue'
 
+
 import 'bulma/css/bulma.css'
+
+Vue.prototype.$http = axios
 
 new Vue({
   el: '#app',
